9ea7b0923d9b830adbc8d7cb09bc52ea07594549
unknown <Selvenchi>
2024-10-31 19:56:23 +0700

    ditambah if/else statement

--- challenges1/challenges1.js
+++ challenges1/challenges1.js
@@ -10,3 +10,15 @@ const BMIJohn = (massJohn / heightJohn) ** 2;
 const markHigherBMI = BMIMark > BMIJohn;
 
 console.log(BMIMark, BMIJohn, markHigherBMI);
+
+if (BMIMark > BMIJohn) {
+  console.log(`Mark's BMI is higher than John's!`);
+} else {
+  console.log(`John's BMI is higher than Mark's!`);
+}
+
+if (BMIMark > BMIJohn) {
+  console.log(`Mark's BMI (${BMIMark}) is higher than Jonh's (${BMIJohn})!`);
+} else {
+  console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})!`);
+}
